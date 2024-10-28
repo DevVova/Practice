280e53e57bafd7d4186956d4886b4f333702ecf8
Vladimir Zabotin <ZabotinV78@gmail.com>
2024-10-28 21:35:48 +0500

    Добавил зависимости для работы с данными из сети okhttp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "1.8.0" // Применяем плагин сериализации
 }
 
 group = "org.example"
@@ -11,6 +12,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    //Библиотека для работы с сетью.
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 }
 
 tasks.test {
